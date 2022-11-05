20f732eae33b1865b3f3dbb88d3e67797ede95c0
vitek74 <vitek74@list.ru>
2022-11-05 19:14:32 +0300

    006

---
+++ 006.js
@@ -0,0 +1,31 @@
+//вместо let использовать const
+
+let myString = 'string'
+
+let myNumber = 456
+
+let myBoolean = true
+
+let myNull = null
+
+let  myUndefined
+
+let myObject = {}
+
+let myArray = []
+
+console.log(myArray)
+
+console.log(myBoolean)
+
+console.log(myNull)
+
+console.log(myNumber)
+
+console.log(myObject)
+
+console.log(myUndefined)
+
+console.log(myString)
+
+
